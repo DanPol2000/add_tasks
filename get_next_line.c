2aa54b9534c9d303e18b60655ee43046871286ff
DanPol2000 <89585637+DanPol2000@users.noreply.github.com>
2021-11-27 18:32:06 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,38 @@
 /*   By: chorse <chorse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:26:15 by chorse            #+#    #+#             */
-/*   Updated: 2021/11/26 18:29:37 by chorse           ###   ########.fr       */
+/*   Updated: 2021/11/27 18:17:50 by chorse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *ft_ost(char *ost)
+char	*ft_ost(char *ost)
 {
-    char *line;
-    int  i;
+	char	*line;
+	int		i;
 
 	line = NULL;
 	i = 0;
-	if (ost[i])
+	while (ost[i] != '\n' && ost[i])
+		i++;
+	if (!ost[i])
 	{
-		while (ost[i] != '\n' && ost[i])
-			i++;
-		line = malloc((ft_strlen(&ost[i + 1]) + 1) * sizeof(char));
-		if (!line)
-			return (NULL);
-		ft_strcpy(line, &ost[i + 1], ft_strlen(&ost[i]));
-		line[ft_strlen(&ost[i + 1])] = '\0';
+		free(ost);
+		return (NULL);
 	}
+	line = malloc((ft_strlen(&ost[i + 1]) + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_strcpy(line, &ost[i + 1], ft_strlen(&ost[i]));
+	line[ft_strlen(&ost[i + 1])] = '\0';
 	free(ost);
 	return (line);
 }
 
-char *ft_line(char *ost)
+char	*ft_line(char *ost)
 {
-    int		i;
+	int		i;
 	char	*line;
 
 	line = NULL;
@@ -45,7 +47,7 @@ char *ft_line(char *ost)
 		while (ost[i] != '\n' && ost[i])
 			i++;
 		i++;
-		line = malloc((i + 1) * sizeof(char));
+		line = malloc((i) * sizeof(char));
 		if (!line)
 			return (NULL);
 		ft_strcpy(line, ost, i);
@@ -54,58 +56,54 @@ char *ft_line(char *ost)
 	return (line);
 }
 
-char *ft_read(int fd, char *ost)
+char	*ft_read(int fd, char *ost)
 {
-	char  *buf;
-    int n_read;
+	char	*buf;
+	int		n_read;
 
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
 		return (NULL);
 	n_read = 1;
 	while (n_read > 0)
 	{
-    	n_read = read(fd, buf, BUFFER_SIZE);
+		n_read = read(fd, buf, BUFFER_SIZE);
+		buf[n_read] = '\0';
 		if (n_read < 0)
 		{
 			free(buf);
 			return (NULL);
 		}
 		if (n_read == 0)
-			break;
+			break ;
 		ost = ft_strjoin(ost, buf);
 		if (!ost)
 		{
 			free(buf);
 			return (NULL);
 		}
-		buf[n_read] = '\0';
 		if (ft_strchr(ost, '\n'))
-			break;
+			break ;
 	}
 	free(buf);
 	return (ost);
 }
 
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char  *res;
-    static char *ost;
-	// char *tmp;
+	char		*res;
+	static char	*ost;
 
 	res = NULL;
-    if ((fd < 0 ) || (BUFFER_SIZE <= 0))
+	if ((fd < 0 ) || (BUFFER_SIZE <= 0))
 		return (NULL);
-	// tmp = ost;
 	ost = ft_read(fd, ost);
 	if (ost)
 	{
 		res = ft_line(ost);
 		ost = ft_ost(ost);
 	}
-	// tmp = ost;
-    return (res);
+	return (res);
 }
 
 int main()
@@ -115,6 +113,4 @@ int main()
     fd = open("text.txt", O_RDONLY);
     printf("res(1) = %s\n", get_next_line(fd));
 	printf("res(2) = %s\n", get_next_line(fd));
-	printf("res(3) = %s\n", get_next_line(fd));
-	printf("res(4) = %s\n", get_next_line(fd));
 }
