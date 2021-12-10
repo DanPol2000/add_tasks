083be9f2cfc3ba05f81ff796620fae15f015366a
DanPol2000 <89585637+DanPol2000@users.noreply.github.com>
2021-12-10 17:48:43 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chorse <chorse@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 15:41:17 by chorse            #+#    #+#             */
+/*   Updated: 2021/12/10 16:56:48 by chorse           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
@@ -6,37 +17,56 @@ int	ft_printf(const char *str, ...)
     va_list args;
     int i;
     int cnt;
+	t_flag flag;
 
     i = 0;
 	cnt = 0;
 	if (str == NULL)
 		return (-1);
     va_start(args, str);
+	ft_flag(&flag);
     while (str[i])
     {
-		if (str[i] != '%' && str[i])
+		while (str[i] != '%' && str[i])
 		{
-			ft_putchar_fd()
+			ft_putchar_fd(str[i], 1);
 			cnt++;
+			i++;
 		}
 		if (!str[i])
-			return (count);
-        if (str[i] == '%' && ft_strchr("cspdiuxX%", str[i + 1]))
-            cnt += ft_flag(args)
+			return (cnt);
+        if (str[i] == '%')
+		{
+			if (str[i + 1] == ' ');
+			{
+				ft_putchar_fd(str[i], 1);
+				cnt++;
+			}
+			ft_parse(str, &flag, args, i);
+		}
         i++;
     }
     va_end(args);
     return (cnt);
 }
 
-int	ft_flag(va_list argv)
+void	ft_flag(t_flag *flag)
 {
-	int		size;
-	char	*tmp;
+	flag->type = 0;
+	flag->width = 0;
+	flag->minus = 0;
+	flag->zero = 0;
+	flag->precision = -1; 
+	flag->star = 0;
+}
 
-	tmp = ft_itoa(va_arg(argv, int));
-	ft_putstr_fd(tmp, 1);
-	size = ft_strlen(tmp);
-	free(tmp);
-	return (size);
+
+int	ft_parse(const char *str, t_flag *flag, va_list args, int i)
+{
+	while (str[i])
+	{
+		if (str[i] == 0)
+			flag->zero = 1;
+		
+	}
 }
--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chorse <chorse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/10 15:43:19 by chorse            #+#    #+#             */
-/*   Updated: 2021/12/10 16:15:35 by chorse           ###   ########.fr       */
+/*   Created: 2021/10/12 18:27:20 by chorse            #+#    #+#             */
+/*   Updated: 2021/12/10 16:15:49 by chorse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FT_PRINTF_H
-#define	FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct s_flag
+void	ft_putchar_fd(char c, int fd)
 {
-	int	type;
-	int	width;
-	int	minus;
-	int	zero;
-	int precision; 
-	int star;
-}	t_flag;
-
-void	ft_putchar_fd(char c, int fd);
-
-#endif
+	write(fd, &c, 1);
+}
